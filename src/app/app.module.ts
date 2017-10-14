dcfcef3e7906cde5fb84e00c5ad854e8e3ef206a
Dolebas <johan.hammarstrm@gmail.com>
2017-10-14 23:33:32 +0200

    Structure basic components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,35 +4,56 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+// AngularFire stuff
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
+// Environment config
 import { Environment } from '../environment';
 
+// Here we are importing components.
+// These are components that will be loaded regardless of route entry?
+// The .component extension is commonly omitted
+// Avoid importing several components per module?
+// Import several modules with child components instead?
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
+
+// ToDo: Create a module file for this component and import the .module instead.
+import { HomePageModule } from '../pages/home/home.module';
+
+// Here we are importing a module.
+import { LoginPageModule } from '../pages/login/login.module';
+
+// import { LoginPageModule } from '../pages/login/login';
 
 @NgModule({
+  // Declarations tells NgModule which components to load?
+  // Declarations are only useable in the current module (app.module covers all modules though).
   declarations: [
     MyApp,
-    HomePage,
-    LoginPage
+    // HomePage
   ],
+  // Imports tells NgModule which modules to load.
+  // Importing modules with it's own declarations will load their respective components.
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(Environment.firebase),
     AngularFirestoreModule.enablePersistence(),
     AngularFireAuthModule,    
+    LoginPageModule,
+    HomePageModule
   ],
+  // Here we are bootstrapping something in the ionic-angular library. Learn more about this..
   bootstrap: [IonicApp],
+  // Entry components are used by Ionic to ensure the offline compiler they should be included
   entryComponents: [
     MyApp,
-    HomePage,
-    LoginPage
+    // HomePage,
   ],
+  // The list of providers declares global services.
+  // These are all ClassNames? with the @Injectable decorator?
   providers: [
     StatusBar,
     SplashScreen,
